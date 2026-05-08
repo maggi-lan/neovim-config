24cc713ea3af905a656156ea1c0fafbb75616072
Mahilan Suki <mahilan.suki@gmail.com>
2026-05-08 12:55:25 +0530

    Enable <C-p> keymap for markdown files only

--- after/plugin/markdown-preivew.lua
+++ after/plugin/markdown-preivew.lua
@@ -1,1 +1,9 @@
-vim.keymap.set("n", "<C-p>", "<cmd>MarkdownPreviewToggle<CR>")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(ev)
+    vim.keymap.set("n", "<C-p>", "<cmd>MarkdownPreviewToggle<CR>", {
+      buffer = ev.buf,
+      desc = "Toggle Markdown Preview",
+    })
+  end,
+})
